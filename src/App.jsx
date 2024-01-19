9668750801392fd19d24605a3462e2f43b6cd541
miawithcode <cuttontail@gmail.com>
2024-01-20 01:36:15 +0800

    feat: components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Home from './components/Home';
+import Modal from './components/Modal';
+import Sidebar from './components/Sidebar';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <main>
+      <Home />
+      <Sidebar />
+      <Modal />
+    </main>
+  );
+};
+export default App;
