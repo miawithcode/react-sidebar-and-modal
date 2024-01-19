ddf994e2f9630b64b540c7dfac07c89acff31821
miawithcode <cuttontail@gmail.com>
2024-01-20 04:38:21 +0800

    feat: modal component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,16 @@ import { useGlobalContext } from '../context';
 import { FaBars } from 'react-icons/fa';
 
 const Home = () => {
-  const {
-    isSidebarOpen,
-    isModalOpen,
-    openSidebar,
-    closeSidebar,
-    openModal,
-    closeModal,
-  } = useGlobalContext();
+  const { openSidebar, openModal } = useGlobalContext();
 
   return (
     <main>
       <button onClick={openSidebar} className="sidebar-toggle">
         <FaBars />
       </button>
-      <button onClick={openModal} className='btn'>Open Modal</button>
+      <button onClick={openModal} className="btn">
+        Open Modal
+      </button>
     </main>
   );
 };
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,17 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from '../context';
+
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
   return (
-    <div>Modal</div>
-  )
-}
-export default Modal
+    <div className={isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'}>
+      <div className="modal-container">
+        <button onClick={closeModal} className="close-modal-btn">
+          <FaTimes />
+        </button>
+        <p>Modal</p>
+      </div>
+    </div>
+  );
+};
+export default Modal;
