18f42166888a4b173111e97d0ba914027e8eb375
miawithcode <cuttontail@gmail.com>
2024-01-20 04:11:00 +0800

    feat: home component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
-    <main>
+    <>
       <Home />
       <Sidebar />
       <Modal />
-    </main>
+    </>
   );
 };
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,23 @@
 import { useGlobalContext } from '../context';
+import { FaBars } from 'react-icons/fa';
 
 const Home = () => {
-  const { openSidebar, openModal } = useGlobalContext();
-  console.log(openModal);
+  const {
+    isSidebarOpen,
+    isModalOpen,
+    openSidebar,
+    closeSidebar,
+    openModal,
+    closeModal,
+  } = useGlobalContext();
 
-  return <div>Home</div>;
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className='btn'>Open Modal</button>
+    </main>
+  );
 };
 export default Home;
